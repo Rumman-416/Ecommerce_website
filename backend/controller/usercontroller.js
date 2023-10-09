29505ddba9151b2232c31e61bd6f0fe6fbdfe849
Rumman <chowdhuryrumman78@gmail.com>
2023-10-09 23:29:40 +0530

    first commit

--- backend/controller/usercontroller.js
+++ backend/controller/usercontroller.js
@@ -2,4 +2,4 @@ const ErrorHandler = require("../utils/errorhandler");
 const catchasyncError = require("../middleware/catchAsyncErr");
 const user = require("../models/userModle");
 
-const app;
+const app = 24;
